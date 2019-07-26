720b49625b6309e54b7e4d175487235d19f5b597
He <1795181887@qq.com>
2019-07-26 17:27:05 +0800

    第一次配路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import  MovieRouter from  '@/router/movie/index'
+import  MineRouter from  '@/router/mine/index'
+import  CinemaRouter from  '@/router/cinema/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    MovieRouter,
+    // MineRouter,
+    // CinemaRouter
   ]
 })
